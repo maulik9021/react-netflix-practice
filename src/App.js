5364cf669c606ecd1638792543975b46929bac60
Maulik Patel <patelmaulik212@gmail.com>
2024-04-19 12:58:05 -0400

    CRA & Tailwindcss

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Lets Make Netflix App</h1>
+        <h1 className="text-3xl font-bold text-green-400">
+          Lets Make Netflix App
+        </h1>
       </header>
     </div>
   );
